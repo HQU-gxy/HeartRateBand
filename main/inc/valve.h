2efa92c1ce67f3bfd3e63bf6a313cc7c203b0c74
crosstyan <crosstyan@outlook.com>
2023-12-04 18:47:20 +0800

    refactor: move valve out

---
+++ main/inc/valve.h
@@ -0,0 +1,140 @@
+//
+// Created by Kurosu Chan on 2023/12/4.
+//
+
+#ifndef PUNCHER_VALVE_H
+#define PUNCHER_VALVE_H
+
+#include "common.h"
+
+namespace peripheral {
+enum class PunchStep {
+  Delay = 0,
+  Out,
+  Stay,
+  Back,
+};
+
+std::string to_string(PunchStep step) {
+  switch (step) {
+    case PunchStep::Delay:
+      return "Delay";
+    case PunchStep::Out:
+      return "Out";
+    case PunchStep::Stay:
+      return "Stay";
+    case PunchStep::Back:
+      return "Back";
+    default:
+      std::unreachable();
+  }
+}
+
+struct ValveOut {
+  bool add;
+  bool decrease;
+};
+
+PunchStep next(PunchStep step) {
+  switch (step) {
+    case PunchStep::Delay:
+      return PunchStep::Out;
+    case PunchStep::Out:
+      return PunchStep::Stay;
+    case PunchStep::Stay:
+      return PunchStep::Back;
+    case PunchStep::Back:
+      return PunchStep::Delay;
+    default:
+      std::unreachable();
+  }
+}
+
+ValveOut valve(PunchStep step) {
+  switch (step) {
+    case PunchStep::Delay:
+      return {0, 0};
+    case PunchStep::Out:
+      return {1, 0};
+    case PunchStep::Stay:
+      return {0, 0};
+    case PunchStep::Back:
+      return {0, 1};
+    default:
+      std::unreachable();
+  }
+}
+
+class Valve {
+public:
+  static constexpr auto TAG = "Valve";
+  using tp_t                = Instant::timepoint_t;
+  using duration_t          = std::chrono::milliseconds;
+
+private:
+  gpio_num_t add_;
+  gpio_num_t decrease_;
+  PunchStep state                                   = PunchStep::Delay;
+  etl::flat_map<PunchStep, duration_t, 4> delay_map = {
+      {PunchStep::Delay, common::DEFAULT_DURATION},
+      {PunchStep::Out, common::DEFAULT_DURATION},
+      {PunchStep::Stay, common::DEFAULT_DURATION},
+      {PunchStep::Back, common::DEFAULT_DURATION},
+  };
+  tp_t disable_time_point = 0;
+  bool is_enabled         = false;
+  Instant instant;
+
+  void action(PunchStep step) {
+    auto [add, decrease] = valve(step);
+    gpio_set_level(add_, add);
+    gpio_set_level(decrease_, decrease);
+  }
+
+  void next_state() {
+    state = next(state);
+  }
+
+  void next_action() {
+    next_state();
+    action(state);
+  }
+
+public:
+  Valve(gpio_num_t add, gpio_num_t decrease) : add_(add), decrease_(decrease) {
+    disable_time_point = esp_timer_get_time();
+  }
+
+  void begin() {
+    pinMode(add_, OUTPUT);
+    pinMode(decrease_, OUTPUT);
+  }
+
+  void set_delay(PunchStep step, duration_t delay) {
+    delay_map[step] = delay;
+  }
+
+  void enable() {
+    is_enabled   = true;
+    auto now     = esp_timer_get_time();
+    auto elapsed = now - disable_time_point;
+    instant.add(elapsed);
+  }
+
+  void disable() {
+    is_enabled         = false;
+    disable_time_point = esp_timer_get_time();
+  }
+
+  void poll() {
+    bool run = instant.elapsed() > delay_map[state];
+    if (run && is_enabled) {
+      next_action();
+      ESP_LOGI(TAG, "state %s", to_string(state).c_str());
+      instant.reset();
+    }
+  }
+};
+}
+
+#endif // PUNCHER_VALVE_H
--- main/src/app_main.cpp
+++ main/src/app_main.cpp
@@ -3,145 +3,18 @@
 #include "LoadCell.h"
 #include "common.h"
 #include <etl/flat_map.h>
-#include <button.h>
+#include "button.h"
+#include "valve.h"
 #include <driver/gpio.h>
 
 using namespace common;
 
 // https://espressif-docs.readthedocs-hosted.com/projects/arduino-esp32/en/latest/api/timer.html#timeralarm
-using tp_t       = decltype(millis());
-using duration_t = std::chrono::milliseconds;
-enum class PunchStep {
-  Delay = 0,
-  Out,
-  Stay,
-  Back,
-};
-
-std::string to_string(PunchStep step) {
-  switch (step) {
-    case PunchStep::Delay:
-      return "Delay";
-    case PunchStep::Out:
-      return "Out";
-    case PunchStep::Stay:
-      return "Stay";
-    case PunchStep::Back:
-      return "Back";
-    default:
-      std::unreachable();
-  }
-}
-
-struct ValveOut {
-  bool add;
-  bool decrease;
-};
-
-PunchStep next(PunchStep step) {
-  switch (step) {
-    case PunchStep::Delay:
-      return PunchStep::Out;
-    case PunchStep::Out:
-      return PunchStep::Stay;
-    case PunchStep::Stay:
-      return PunchStep::Back;
-    case PunchStep::Back:
-      return PunchStep::Delay;
-    default:
-      std::unreachable();
-  }
-}
-
-ValveOut valve(PunchStep step) {
-  switch (step) {
-    case PunchStep::Delay:
-      return {0, 0};
-    case PunchStep::Out:
-      return {1, 0};
-    case PunchStep::Stay:
-      return {0, 0};
-    case PunchStep::Back:
-      return {0, 1};
-    default:
-      std::unreachable();
-  }
-}
-
-class Valve {
-public:
-  static constexpr auto TAG = "Valve";
-
-private:
-  gpio_num_t add_;
-  gpio_num_t decrease_;
-  PunchStep state                                   = PunchStep::Delay;
-  etl::flat_map<PunchStep, duration_t, 4> delay_map = {
-      {PunchStep::Delay, DEFAULT_DURATION},
-      {PunchStep::Out, DEFAULT_DURATION},
-      {PunchStep::Stay, DEFAULT_DURATION},
-      {PunchStep::Back, DEFAULT_DURATION},
-  };
-  tp_t disable_time_point = 0;
-  bool is_enabled         = false;
-  Instant instant;
-
-  void action(PunchStep step) {
-    auto [add, decrease] = valve(step);
-    gpio_set_level(add_, add);
-    gpio_set_level(decrease_, decrease);
-  }
-
-  void next_state() {
-    state = next(state);
-  }
-
-  void next_action() {
-    next_state();
-    action(state);
-  }
-
-public:
-  Valve(gpio_num_t add, gpio_num_t decrease) : add_(add), decrease_(decrease) {
-    disable_time_point = esp_timer_get_time();
-  }
-
-  void begin() {
-    pinMode(add_, OUTPUT);
-    pinMode(decrease_, OUTPUT);
-    is_enabled = true;
-  }
-
-  void set_delay(PunchStep step, duration_t delay) {
-    delay_map[step] = delay;
-  }
-
-  void enable() {
-    is_enabled   = true;
-    auto now     = esp_timer_get_time();
-    auto elapsed = now - disable_time_point;
-    instant.add(elapsed);
-  }
-
-  void disable() {
-    is_enabled         = false;
-    disable_time_point = esp_timer_get_time();
-  }
-
-  void poll() {
-    bool run = instant.elapsed() > delay_map[state];
-    if (run && is_enabled) {
-      next_action();
-      ESP_LOGI(TAG, "state %s", to_string(state).c_str());
-      instant.reset();
-    }
-  }
-};
 
 extern "C" [[noreturn]] void app_main(void) {
   initArduino();
   static auto sensor    = LoadCell{pin::D_OUT, pin::DP_SCK};
-  static auto valve     = Valve{pin::VALVE_ADD, pin::VALVE_DECREASE};
+  static auto valve     = peripheral::Valve{pin::VALVE_ADD, pin::VALVE_DECREASE};
   static auto punch_btn = peripheral::EdgeButton{pin::PUNCH_BTN};
   pinMode(pin::LED, OUTPUT);
   digitalWrite(pin::LED, HIGH);
