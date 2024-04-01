ddd8db7c07c002faad5282a095068740c3c36c81
crosstyan <crosstyan@outlook.com>
2024-04-01 12:44:50 +0800

    more ana and data

--- scripts/serve.py
+++ scripts/serve.py
@@ -1,5 +1,6 @@
 import socket
 from tokenize import Single
+from altair import sample
 import numpy as np
 from jaxtyping import Int, Float
 from loguru import logger
@@ -12,6 +13,7 @@ from anyio.to_thread import run_sync
 from threading import Thread
 import click
 import cbor2 as cbor
+import json
 from dataclasses import dataclass
 from pydantic import BaseModel
 import streamlit as st
@@ -23,10 +25,11 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 from fastparquet import write
 from plotly.graph_objects import Scatter
-from typing import Any, Dict, Tuple, List, TypedDict
+from typing import Any, Dict, Optional, Tuple, List, TypedDict
 from time import time
 from typeguard import typechecked, check_type
 from npy_append_array import NpyAppendArray
+from pathlib import Path
 import npy_append_array
 
 NDArray = np.ndarray
@@ -36,16 +39,26 @@ ArrayType = Int[NDArray, "... 2"]
 DEFAULT_HOST = "0.0.0.0"
 DEFAULT_PORT = 8080
 
-BATCH_SIZE = 5_000
+
+class PulseData(TypedDict):
+    sample_rate: int | float
+    data: NDArray
 
 
 async def run_udp(host: str, port: int,
-                  channel: MemoryObjectSendStream[NDArray]):
+                  channel: MemoryObjectSendStream[PulseData]):
     logger.info(f"Listening on {host}:{port}")
     # https://stackoverflow.com/questions/64791558/create-parquet-files-from-stream-in-python-in-memory-efficient-manner
     # https://github.com/dask/fastparquet/issues/476
     schema = pa.schema([("red", pa.uint32())])
     acc = np.zeros((0, 2), dtype=np.uint32)
+    WINDOW_SIZE_SEC = 10
+    # will be set after the first data arrives
+    # as a reference value
+    sr: Optional[int] = None
+    # remove data.npy if it exists
+    if Path("data.npy").exists():
+        Path("data.npy").unlink()
 
     async def sock_recv_gen(sock: UDPSocket):
         while True:
@@ -60,20 +73,27 @@ async def run_udp(host: str, port: int,
                 data: any = cbor.loads(b)  # type: ignore
                 header = list(data[0])
                 strides = list(header[0])
+                # I expect the sample rate to be consistent
                 sample_rate = header[1]
+                if sr is None:
+                    sr = sample_rate
+                assert sr == sample_rate, f"sample rate inconsistency: {sr} != {sample_rate}"
+                batch_size = WINDOW_SIZE_SEC * sample_rate
                 logger.info(
                     f"len(data)={len(b)} from {addr}; strides={strides}; sample_rate={sample_rate}"
                 )
                 arr = np.array(data[1]).reshape((-1, strides[1]))
-                await channel.send(arr)
+                await channel.send({"sample_rate": sample_rate, "data": arr})
                 acc = np.vstack((acc, arr))
                 count = acc.shape[0]
-                if count >= BATCH_SIZE:
+                if count >= batch_size:
 
                     def save_as_np():
                         logger.info("write chunk to data.npy")
                         with NpyAppendArray("data.npy") as npaa:
                             npaa.append(acc)
+                        with open("data.npy.json", "w") as f:
+                            json.dump({"sample_rate": sr}, f)
 
                     await run_sync(save_as_np)
                     acc = np.zeros((0, 2), dtype=np.uint32)
@@ -87,7 +107,7 @@ async def run_udp(host: str, port: int,
 # https://github.com/streamlit/streamlit/issues/1792
 
 
-def st_main(channel: MemoryObjectReceiveStream[NDArray]):
+def st_main(channel: MemoryObjectReceiveStream[PulseData]):
     logger.info("Streamlit main")
     st.title("Pulse")
     red_chart = st.empty()
@@ -111,14 +131,18 @@ def st_main(channel: MemoryObjectReceiveStream[NDArray]):
     # for 800 samples per second
     # interval 1.25
     for data in sync_iter_channel():
-        WINDOW_SIZE = 3000
-        acc = np.vstack((acc, data))
+        WINDOW_SIZE_SEC = 10
+        sample_rate = data["sample_rate"]
+        window_size_sample = WINDOW_SIZE_SEC * sample_rate
+        sample_interval = 1 / sample_rate
+        arr = data["data"]
+        acc = np.vstack((acc, arr))
         count = acc.shape[0]
-        if count > WINDOW_SIZE:
-            acc = acc[-WINDOW_SIZE:]
+        if count > window_size_sample:
+            acc = acc[-window_size_sample:]
         red = acc[:, 0]
         ir = acc[:, 1]
-        xs = np.arange(count) * 1.25
+        xs = np.arange(len(red)) * sample_interval
 
         red_fig = {
             "data": Scatter(x=xs, y=red, name="Red"),
@@ -145,7 +169,7 @@ class Params:
 
 class AppState(TypedDict):
     thread: Thread
-    receiver: MemoryObjectReceiveStream[NDArray]
+    receiver: MemoryObjectReceiveStream[PulseData]
 
 
 # https://discuss.streamlit.io/t/how-to-have-global-variable-outside-of-session-state/54885/2
@@ -153,7 +177,7 @@ class AppState(TypedDict):
 # https://docs.streamlit.io/library/advanced-features/caching
 @st.cache_resource
 def init(params: Params) -> AppState:
-    sender, receiver = create_memory_object_stream[NDArray]()
+    sender, receiver = create_memory_object_stream[PulseData]()
     t = Thread(target=run, args=(run_udp, params.host, params.port, sender))
     t.start()
     return {"thread": t, "receiver": receiver}
